f40d5c1da36dfbc31091862828cbf9fb8bbc1ba3
Etignis <etignis@gmail.com>
2019-04-28 18:03:43 +0300

    fix[data]

--- data/allItems.js
+++ data/allItems.js
@@ -746,8 +746,7 @@ var allItems = [
 			"name": "TOMB OF LEVISTUS",
 			"text": "As a reaction when you take damage, you can entomb yourself in ice, which melts away at the end of your next turn. You gain 10 temporary hit points per warlock level, which take as much of the triggering damage as possible. Immediately after you take the damage, you gain vulnerability to fire damage, your speed is reduced to 0, and you are incapacitated. These effects, including any remaining temporary bit points, all end when the ice melts.<br> Once you use this invocation, you can't use it again until you finish a short or long rest.",
 			"source": "XGTE",
-			"minLevel": "5",
-			"spellType": "hex"
+			"minLevel": "5"
 		},
 		"ru": {
 			"name": "Гробница Левистуса",
